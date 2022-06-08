a8f43a3794186d2c38b5a3b77f20a49fc373e176
junaideko <junaidekoraimot@gmail.com>
2022-06-08 11:02:06 -0700

    only diffent elements

--- 0x04-python-more_data_structres/3-common_elements.py
+++ 0x04-python-more_data_structres/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    commonElements = {x for x in set_1 if x in set_2}
+    #commonElements = {x for x in set_1 if x in set_2}
+    commonElements = set_1 & set_2
     return commonElements
---
+++ 0x04-python-more_data_structres/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    eleinBothbutnotcom = set_1 ^ set_2
+    return(eleinBothbutnotcom)
