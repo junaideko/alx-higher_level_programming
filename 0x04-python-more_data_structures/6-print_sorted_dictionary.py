1867d604664f05edb1b88779344a59f7637bf09d
junaideko <junaidekoraimot@gmail.com>
2022-06-08 21:11:16 -0700

    simple delete a key

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for k in sorted(a_dictionary.keys()):
-        print(f"{k}: {a_dictionary.get(k)}")
+    for k, y in sorted(a_dictionary.items()):
+        print(f"{k}: {y}".format(k, y))
