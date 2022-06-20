55befe23a5252bea53e30b917a262f5244e4775f
junaideko <junaidekoraimot@gmail.com>
2022-06-20 12:22:31 -0700

    safe printinf og int list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     elemen = 0
-    for y in range(x):
+    for y in range(0, x):
         try:
             print(my_list[y], end="")
             elemen += 1
