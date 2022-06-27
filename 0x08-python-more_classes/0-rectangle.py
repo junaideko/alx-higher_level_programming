494932d4f7dc034162d7bb1ea36758535a465582
junaideko <junaidekoraimot@gmail.com>
2022-06-27 13:38:57 -0700

    simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module writes an empty class named rectangle"""
+
+
+class Rectangle:
+    """an empty class that defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""module that defines a rectangle by width"""
+
+
+def __init(self, width=0):
+    """create an instance width"""
