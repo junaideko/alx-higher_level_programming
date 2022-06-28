1170eb83427218ac44ec9d86a9b5d6e24f8a0892
junaideko <junaidekoraimot@gmail.com>
2022-06-28 03:49:34 -0700

    a square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
 
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -83,7 +83,7 @@ class Rectangle:
         and decrements number of instances of rectangle
         """
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        type(self).number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
