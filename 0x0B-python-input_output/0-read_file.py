51c3636f66e455c587f40a84cb67498e6f0f79b6
junaideko <junaidekoraimot@gmail.com>
2022-07-05 13:03:26 -0700

    open a file with with statemtn

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Reads a text file
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+    Args:
+        filename (str): name of the file to read
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
