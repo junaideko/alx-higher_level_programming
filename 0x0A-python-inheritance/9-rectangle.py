bba62f42c4378dc65834f6a113581bf72fcf0320
junaideko <junaidekoraimot@gmail.com>
2022-07-05 01:20:58 -0700

    full rectangle and inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,7 +12,19 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
-        self.__height = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        """String representatin of area value"""
+        # return "[Rectangle] {:d}/{:d}"
+        # .format(self.__width, self.__height), end=""
+        print("[Rectangle] {:d}/{:d}"
+              .format(self.__width, self.__height), end="")
+        return ""
