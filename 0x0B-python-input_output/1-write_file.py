2ad41773801eeac6e2a3f8e0780cf7c15441e968
junaideko <junaidekoraimot@gmail.com>
2022-07-05 13:18:08 -0700

    write string to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8)
+    Args:
+        filename (str): name of the file to write
+        text (str): text to write to the file
+    Returns:
+        number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return (file.write(text))
