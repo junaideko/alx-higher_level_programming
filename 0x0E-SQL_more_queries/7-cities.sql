d86859cc3147d2082c74e8aaa106ad98a2545520
junaideko <junaidekoraimot@gmail.com>
2022-08-11 06:13:04 -0700

    create a database and a table, with three colums, one of which is a foreign key that reference another table columnid

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on my MySQL server.
+-- table has three columns:id INT(unique, auto generated, can’t be null and is a primary key)
+-- state_id INT (can’t be null and is be a FOREIGN KEY that references to id of the states table)
+-- name VARCHAR(256) (can’t be null)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
