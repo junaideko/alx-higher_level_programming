19907069dbdc27d78dc74a5a2215fd1da2b94115
junaideko <junaidekoraimot@gmail.com>
2022-08-11 05:30:11 -0700

    create table with one of its column set to a defaukt value

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Creates the table id_not_null on my MySQL server
+-- with description: id INT (with the default value 1) name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+);
