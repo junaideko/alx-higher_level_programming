3ac59bb4c5cd2d0850c6f1db41bf4c6a9071dacf
junaideko <junaidekoraimot@gmail.com>
2022-09-07 13:43:08 -0700

    fix semistandard error

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,12 +4,13 @@ module.exports = class Rectangle {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
     this.width = w;
     this.height = h;
-}
-print () {
-  for (let i = 0; i < this.height; i++) {
-    let a = '';
-    for (let j = 0; j < this.width; j++) a += 'X';
-    console.log(a);
   }
- }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let a = '';
+      for (let j = 0; j < this.width; j++) a += 'X';
+      console.log(a);
+    }
+  }
 };
