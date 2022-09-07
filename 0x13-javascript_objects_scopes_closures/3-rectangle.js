9479a5ed3e96204585938ee7dc32da242fe28a68
junaideko <junaidekoraimot@gmail.com>
2022-09-07 13:23:36 -0700

    add method instance

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
+    this.width = w;
+    this.height = h;
+}
+print () {
+  for (let i = 0; i < this.height; i++) {
+    let a = '';
+    for (let j = 0; j < this.width; j++) a += 'X';
+    console.log(a);
+  }
+ }
+};
